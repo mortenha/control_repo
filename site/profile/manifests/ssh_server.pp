c0e4fb4d7967a8455ff34a16d4a5d8474ca085b8
Morten Hansen <mortens.epost@gmail.com>
2020-01-01 12:34:40 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,3 +1,15 @@
 class profile::ssh_server {
-  
+  package {'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqC741DmNH29RMgmHr/IgttSzhSOj3slyQ4LIqC3GNbHE9klGrAVMw/z3B37LbFgvkoxvi6v7dlegx25J5BQ/dcqoS9Rb6UYVjPCpGG1bKuKqHZDmLe4H69LVclySsfpg2Hgq8QWhBVvMrmkEVSLwaGXvyBc+SHzfHskYyvaf+HDPAz0Q3IEMt3eSUTznTSBxWYEmBp40DRnQnKS+/FhZfMmpa+Ri3Jk5KVHjMy7wgVbKWqCLra4s/UfE255IpRmcbxQEKK6MdcLmez51hbQwHt1gvRzMBN+Lws0PmjWUqGB/zuLFg9H8pdJB+tNnF6d+HB9OwRmxefNdFBPx0m8Vd',
+  }
 }
