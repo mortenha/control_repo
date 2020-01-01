2d402b3245b3c0eb247e82aa1db19c019477519a
Morten Hansen <mortens.epost@gmail.com>
2020-01-01 13:42:37 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
   $install_dir = '/opt/minecraft'
  ) {
-  file {'/opt/minecraft':
+  file { $install_dir:
     ensure => directory,
   }
   file {"${install_dir}/minecraft_server.jar":
