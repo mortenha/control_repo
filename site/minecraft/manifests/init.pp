e5a43dc227626240dbe82c6bec948ce87559c337
Morten Hansen <mortens.epost@gmail.com>
2020-01-01 13:29:02 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,18 @@
 class minecraft {
+  $url = 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar'
+  $install_dir = '/opt/minecraft'
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {"${install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
+    source => $url,
     before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true'
   }
@@ -21,6 +23,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']],
   }
 }
